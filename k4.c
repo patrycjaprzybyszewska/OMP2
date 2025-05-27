0b57c5753fff935bc945b0f9421c4e9cf991f2ff
patrycjaprzybyszewska <126480017+patrycjaprzybyszewska@users.noreply.github.com>
2025-05-28 00:14:21 +0200

    Update k4.c

--- k4.c
+++ k4.c
@@ -10,14 +10,14 @@ int main() {
     int m = 2;   //100000000
     int n = 50000000;     //2000000   
 
-    clock_t start = clock();
+   
 
     bool* result = (bool*)malloc((n - m + 1) * sizeof(bool));
     memset(result, true, (n - m + 1) * sizeof(bool));
 
     bool* primeArray = (bool*)malloc((sqrt(n) + 1) * sizeof(bool));
     memset(primeArray, true, (sqrt(n) + 1) * sizeof(bool));
-
+ clock_t start = clock();
     for (int i = 2; i*i*i*i <= n; i++) {
         if (primeArray[i] == true) {
             for (int j = i*i; j*j <= n; j+=i) {
@@ -27,7 +27,7 @@ int main() {
     }
 
     int sqrt1 = sqrt(n);
-#pragma omp parallel for schedule(dynamic)
+#pragma omp parallel for 
     for (int i = 2; i <= sqrt1; i++) {
         if (primeArray[i]) {
             int firstMultiple = (m / i);
@@ -54,7 +54,7 @@ int main() {
     //         printf("%d jest pierwsza\n", i);
     //     }
     // }
-
+ clock_t end = clock(); 
     int count = 0;
     for (int i = m; i <= n; i++) {
         if (result[i - m]) {
@@ -63,7 +63,7 @@ int main() {
     }
     printf("Liczb pierwszych: %d\n", count);
 
-    clock_t end = clock(); 
+   
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
     printf("Czas wykonania: %.3f sekund\n", time_spent);
